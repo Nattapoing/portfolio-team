ea93562dd0448f97fcb349fba8cb718de4ff1385
Nattapoing <66160124@go.buu.ac.th>
2025-01-26 19:46:42 +0700

    feat: เพิ่ม navbar และ hero section

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+  extend: {
+    colors: {
+      'primary': '#1a73e8',
+      'secondary': '#34a853',
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        },
+        fontFamily: {
+        sans: ['Prompt', 'sans-serif'],
+        }
+  },
+  },
+  plugins: [],
+  }
+  
